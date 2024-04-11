719287cb71a62a2e3c70dc2ee4d370611af42b57
MichBell742 <149068520+MichBell742@users.noreply.github.com>
2024-04-11 13:36:34 +0200

    fine file compressioneRLE

--- Compressione/CompressioneRLE.java
+++ Compressione/CompressioneRLE.java
@@ -12,7 +12,7 @@ public class CompressioneRLE extends Application{
 
 	char immagine[][]= {{'v','b','b'},
 						{'g','g','g'},
-						{'b','v','n'}};
+						{'b','c','d'}};
 	
 	Label pesoNonCompresso= new Label("-----------------");
 	Label pesoCompresso= new Label("-----------------");
@@ -50,21 +50,47 @@ public class CompressioneRLE extends Application{
 	}
 	
 	public void calcolaDati() {
-		int pesoTotale= 3*3*24;
+		double pesoTotale= immagine.length*immagine[0].length*24;
 		int primoElemento=immagine[0][0];
-		int contaRipetizioni=1;
+		int contaRipetizioni=0;
 		int pesoTotaleCompresso=0;
 		for(int i=0; i<3; i++) {
-			for(int j=1; j<3;j++) {
+			for(int j=0; j<3;j++) {
+				System.out.println(immagine[i][j]);
 				if(primoElemento==immagine[i][j]) {
 					contaRipetizioni++;
+					System.out.println("corrisponde "+contaRipetizioni);
 				}else {
-					pesoTotaleCompresso=contaRipetizioni+24;
+					int lunghezza=converti(contaRipetizioni).length();
+					pesoTotaleCompresso+=lunghezza+24;
+					contaRipetizioni=1;
+					primoElemento=immagine[i][j];
 				}
 			}
 		}
+		int lunghezza=converti(contaRipetizioni).length();
+		pesoTotaleCompresso+=lunghezza+24;
+		System.out.println("finale "+ pesoTotaleCompresso);
+		pesoNonCompresso.setText(""+pesoTotale);
+		pesoCompresso.setText(""+pesoTotaleCompresso);
+		double risultato=100-((pesoTotaleCompresso/pesoTotale)*100);
+		rate.setText(""+risultato);
 	}
-	
+	public String converti(int numeroConvertire) {
+		int base=2;
+		int numero=numeroConvertire;
+		int resto=0;
+		String numeroConvertito="";
+		
+		while(numero!=0) {
+			numero=numeroConvertire/base;
+			resto=numeroConvertire-numero*base;
+			numeroConvertire=numero;
+			numeroConvertito=resto+numeroConvertito;
+			
+		}
+		return numeroConvertito;
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
